f0123b122a12949659e8f01ffcf5e63d0572a4f1
Khairul <khairulihsannudin@mail.ugm.ac.id>
2024-05-24 14:02:13 +0700

    feat(auth): add email verification and fix authentication schema

--- api/main.py
+++ api/main.py
@@ -1,7 +1,10 @@
+from starlette.middleware.sessions import SessionMiddleware
 from fastapi import FastAPI
 from auth.auth import app as auth_router
 from payment.payment import app as payment_router
 
+
 app = FastAPI()
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.add_middleware(SessionMiddleware, secret_key="secret")
+app.include_router(auth_router, prefix="", tags=["auth"])
 app.include_router(payment_router, prefix="/payment", tags=["payment"])
--- api/schema.py
+++ api/schema.py
@@ -14,7 +14,14 @@ class User(SQLModel, table=True):
     username: str
     email: str
     password: str
+    balance: int = 0
+    def to_safe_dict(self):
+        return {key: value for key, value in self.__dict__.items() if key != 'password'}
 
+class Payment(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int | None = Field(default=None, foreign_key='user.id')
+    order_id: str
 
 class Container(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
