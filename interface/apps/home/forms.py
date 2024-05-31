78689b248407562d48c54f7e675d8887864914d2
unknown <asyrafnurardliansyah@mail.ugm.ac.id>
2024-05-31 21:36:08 +0700

    Update UI, changed template

--- interface/apps/home/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import Container
-
-class ContainerForm(forms.ModelForm):
-    class Meta:
-        model = Container
-        fields = ['name', 'image', 'usage_time']
